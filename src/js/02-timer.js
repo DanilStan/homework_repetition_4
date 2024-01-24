58a6171da5a799b4727b3512dcbf6da202e21af4
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-24 17:56:41 +0200

    Update clock

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,8 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 
+let currentDate = null;
+let intervalId = null;
 const startBtn = document.querySelector('button[data-start]');
 const dateNow = Date.now();
 const options = {
@@ -13,9 +15,38 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0].getTime() > dateNow) {
       startBtn.disabled = false;
+      currentDate = selectedDates[0];
+      console.log(currentDate);
     }
   },
 };
 
 startBtn.disabled = true;
+startBtn.addEventListener('click', onClickBtn);
+
 flatpickr('#datetime-picker', options);
+
+function onClickBtn() {
+  intervalId = setInterval(() => {
+    console.log(currentDate - 1000);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
