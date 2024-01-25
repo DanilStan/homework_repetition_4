642b0d7dda34f933ab996f916c41308f062c4c17
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-25 14:51:27 +0200

    Clear code

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,6 +6,8 @@ import 'flatpickr/dist/flatpickr.min.css';
 let currentDate = null;
 let intervalId = null;
 const startBtn = document.querySelector('button[data-start]');
+startBtn.disabled = true;
+startBtn.addEventListener('click', onClickBtn);
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -27,17 +29,12 @@ const refs = {
   secondsRef: document.querySelector('[data-seconds]'),
 };
 
-startBtn.disabled = true;
-startBtn.addEventListener('click', onClickBtn);
-
 flatpickr('#datetime-picker', options);
 
 function onClickBtn() {
   calculeteTime();
   intervalId = setInterval(() => {
     calculeteTime();
-    // const timeForEnd = currentDate - dateNow;
-    // console.log(timeForEnd - 1000);
   }, 1000);
 }
 
@@ -46,7 +43,6 @@ function calculeteTime() {
   const dateNow = new Date();
   const timeForEnd = currentDate - dateNow;
   const resultTime = convertMs(timeForEnd);
-  console.log(resultTime);
 
   refs.daysRef.textContent = resultTime.days;
   refs.hoursRef.textContent = resultTime.hours;
@@ -55,19 +51,14 @@ function calculeteTime() {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = pad(Math.floor(ms / day));
-  // Remaining hours
   const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
